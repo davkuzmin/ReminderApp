1dfa78f0fae82d486061888f43cefa31fc7268b5
davkuzmin <davkuzmin@gmail.com>
2017-03-09 15:17:44 -0500

    Minor tweak
    added additional check when no reminders exist in database

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,7 +1,8 @@
 export default class Utils {
     static ObjToArray(val: Object) {
       var res = [];
-      Object.keys(val).forEach((key) => {
+
+      Object.keys(val || []).forEach((key) => {
         res.push(val[key]);
       });
 
