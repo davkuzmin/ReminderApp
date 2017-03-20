b359470615c4489e1abb582b8934eb9e86229f50
davkuzmin <dkuzmin@email.sc.edu>
2017-03-19 21:06:16 -0400

    Navigation fix

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { RegisterPage } from '../register/register';
 import { TabsPage } from '../tabs/tabs';
 
@@ -18,13 +18,17 @@ export class LoginPage {
 
   constructor(public navCtrl: NavController,
     private toastCtrl: ToastController,
-    private auth: FireAuthService) { }
+    private auth: FireAuthService,
+    private zone: NgZone) { }
 
   login() {
     let self = this;
-    
-    this.auth.login(this.email, this.password).then(() => {
-      this.navCtrl.setRoot(TabsPage, {}, {animate: false});
+
+    return firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(() => {
+      //https://forum.ionicframework.com/t/after-setroot-tabspage-the-default-tab-page-appears-twice/71770/10
+      this.zone.run(() => {
+        this.navCtrl.setRoot(TabsPage, {}, {animate: false});
+      });
     }).catch(function(error) {
       this.invalidLogin();
     });
