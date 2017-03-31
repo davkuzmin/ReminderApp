b4676b5baa61fb57ea6fe926b5e8c0e3ef9fbe72
kmsalah <salah4501@gmail.com>
2017-03-31 00:06:42 -0400

    addReminder Button on Popover
    Added the addReminder function to the popover.ts and added the
    addReminder function to the addReminder button on popover.html

--- src/pages/reminders/popover.ts
+++ src/pages/reminders/popover.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { ViewController } from 'ionic-angular';
+
+import { NavController, AlertController } from 'ionic-angular';
+import { AddReminder } from '../add-reminder/add-reminder';
+
+
 @Component({
   templateUrl: 'popover.html'
 })
 
 export class PopoverContentPage {
-constructor(public viewCtrl: ViewController) {}
+constructor(public viewCtrl: ViewController,public navCtrl: NavController) {}
 close() {
     this.viewCtrl.dismiss();
+  }
+
+
+  addReminder() {
+    this.navCtrl.push(AddReminder);
   }
 }
